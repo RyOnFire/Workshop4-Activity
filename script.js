3432bb61acfc1b99730544aa8b1bf93e7101f234
zmaraj <zsmaraj99@gmail.com>
2026-03-22 20:49:28 -0400

    added files

---
+++ script.js
@@ -0,0 +1,13 @@
+const title = document.getElementById("title");
+const message = document.getElementById("message");
+const output = document.getElementById("output");
+const nameInput = document.getElementById("nameInput");
+
+const changeTextBtn = document.getElementById("changeTextBtn");
+const greetBtn = document.getElementById("greetBtn");
+const colorBtn = document.getElementById("colorBtn");
+
+// Starter example
+changeTextBtn.addEventListener("click", () => {
+  message.textContent = "The text has been changed!";
+});
